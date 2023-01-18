439434c776b52c681d3d7939ffd440b4d6c5d10a
aidehn <115724666+aidehn@users.noreply.github.com>
2023-01-18 17:48:18 +0000

    feat: removed files

--- server/files_to_delete/jwt.js
+++
@@ -1,8 +0,0 @@
-exports.createToken = (id) => {
-  return token;
-};
-
-exports.verifyToken = (token) => {
-  const verification = jwt.verify(token, process.env.TOKEN_SECRET);
-  return verification;
-};
